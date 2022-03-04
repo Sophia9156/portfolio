484adf596f35f5ffc742b4a648191b7c44f263aa
Sophia9156 <suhyun9156@gmail.com>
2022-03-04 17:51:22 +0900

    finish header and footer

--- js/header-footer.js
+++ js/header-footer.js
@@ -4,6 +4,10 @@ $('footer').load('header-footer.html .footer', footerFun);
 function headerFun() {
   $('.header h1').on('click', function() {
     $('.subMenu').toggleClass('active');
+  });
+  $('.burger').on('click', function() {
+    $('.mainMenu').toggleClass('active');
+    $('.burger').toggleClass('active');
   })
 }
 
